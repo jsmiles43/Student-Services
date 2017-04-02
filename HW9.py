56b7e121adaff71ff03b0c29addb748e8efdcdd5
Joe <jsmiles43@gmail.com>
2017-04-02 16:33:21 -0400

    major and clases left functionality

--- HW9.py
+++ HW9.py
@@ -13,14 +13,10 @@ from prettytable import PrettyTable
 class Repository:
     
     def __init__(self):
-        self.student_list = list()
-        self.instructor_list = list()
+        self.student_list = collections.defaultdict(Student)
+        self.instructor_list = collections.defaultdict(Instructor)
+        self.major_dict = collections.defaultdict(set)
         
-    def findUser(self, CWID):
-        user_list = self.student_list + self.instructor_list
-        for user in user_list:
-            if user.CWID == CWID:
-                return user
             
     
     def loadUserInfo(self, file_name, user_type):
@@ -32,9 +28,9 @@ class Repository:
             for line in file:
                 CWID, name, dept = line.strip().split('\t')  
                 if user_type == 'S':
-                    self.student_list.append(Student(CWID, name, dept))
+                    self.student_list[CWID] = Student(CWID, name, dept)
                 elif user_type == 'I':
-                    self.instructor_list.append(Instructor(CWID, name, dept))
+                    self.instructor_list[CWID] = Instructor(CWID, name, dept)
                 
     def loadGrades(self, file_name):
         try:
@@ -44,25 +40,38 @@ class Repository:
         else:
             for line in file:
                 CWID, course_name, grade, i_CWID = line.strip().split('\t')
-                self.findUser(CWID).classes_taken[course_name] = grade
-                self.findUser(i_CWID).courses_taught[course_name] += 1
+                self.student_list[CWID].classes_taken[course_name] = grade
+                self.instructor_list[i_CWID].courses_taught[course_name] += 1
             
+    def loadMajors(self, file_name):
+        try:
+            file = open(file_name, 'r')
+        except FileNotFoundError:
+            print('Can’t open,', file_name, 'current directory is', os.getcwd())
+        else:
+            for line in file:
+                dept, course = line.strip().split('\t')
+                self.major_dict[dept].add(course)
+            for cwid in self.student_list:
+                self.student_list[cwid].classes_left = self.major_dict[self.student_list[cwid].dept] - set(self.student_list[cwid].classes_taken.keys())
+                
+
     def printRepo(self):
         print('Student Summary')
         table = PrettyTable()
-        table.field_names = ['CWID', 'NAME', 'Completed Courses']
+        table.field_names = ['CWID', 'NAME', 'Completed Courses', 'Classes Left']
         table.align = 'l'
-        for student in self.student_list:
-            table.add_row([student.CWID, student.name, sorted((list(dict(student.classes_taken).keys())))])
-        
+        for CWID in self.student_list:
+            table.add_row([CWID, self.student_list[CWID].name, sorted((list(dict(self.student_list[CWID].classes_taken).keys()))), self.student_list[CWID].classes_left])
+        print(table)
         
         print('Instructor Summary')
         table2 = PrettyTable(['CWID', 'NAME', 'Dept', 'Courses', 'Students'])
         table2.align = 'l'
-        for instructor in self.instructor_list:
-            for course in instructor.courses_taught.keys():
-                table2.add_row([instructor.CWID, instructor.name, instructor.dept, course, instructor.courses_taught[course]])
-        print(table)
+        for CWID in self.instructor_list:
+            for course in self.instructor_list[CWID].courses_taught.keys():
+                table2.add_row([CWID, self.instructor_list[CWID].name, self.instructor_list[CWID].dept, course, self.instructor_list[CWID].courses_taught[course]])
+       
         print(table2)
                 
             
@@ -78,7 +87,8 @@ class Student(User):
     def __init__(self, _CWID, _name, _dept):
         super(Student, self).__init__(_CWID, _name, _dept)
         self.classes_taken = collections.defaultdict(str)
-
+        self.classes_left = set()
+        
 class Instructor(User):
     '''An extension of the User Class to create instructor objects'''
     def __init__(self, _CWID, _name, _dept):
@@ -90,6 +100,7 @@ def main():
     repo.loadUserInfo('instructors.txt', 'I');
     repo.loadUserInfo('students.txt', 'S');
     repo.loadGrades('grades.txt')
+    repo.loadMajors('majors.txt')
     repo.printRepo()
     
     
--- HW9test.py
+++ HW9test.py
@@ -9,35 +9,34 @@ Created on Tue Mar 28 12:07:29 2017
 import HW9 as f, unittest
 
 class repoTest(unittest.TestCase):
-    def test_finduser(self):
-        repo = f.Repository()
-        repo.instructor_list = [f.Instructor('98560', 'A', 'SSW') , f.Instructor('98561', 'B', 'SSB'), f.Instructor('98562', 'C', 'SSA')]
-        self.assertTrue(repo.findUser('98560'))
-        self.assertTrue(repo.findUser('98561'))
-        self.assertFalse(repo.findUser('98559'))
 
     def test_loadUserInfoInstructor(self):
         repo = f.Repository()
         repo.loadUserInfo('instructors.txt', 'I')
         self.assertTrue(len(repo.instructor_list) != 0)
-        self.assertIsInstance(repo.instructor_list[0], f.Instructor)
-    
+        self.assertIsInstance(list(repo.instructor_list.values())[0], f.Instructor)
+     
         
     def test_loadUserInfoStudent(self):
         repo = f.Repository()
         repo.loadUserInfo('students.txt', 'S')
         self.assertTrue(len(repo.student_list) != 0)
-        self.assertIsInstance(repo.student_list[0], f.Student)
+        self.assertIsInstance(list(repo.student_list.values())[0], f.Student)
 
     def test_loadGrades(self):
         repo = f.Repository()
         repo.loadUserInfo('instructors.txt', 'I')
         repo.loadUserInfo('students.txt', 'S')
         repo.loadGrades('grades.txt')
-        self.assertTrue(len(repo.student_list[0].classes_taken) != 0)
-        self.assertTrue(len(repo.instructor_list[0].courses_taught) != 0)
+        self.assertTrue(len(repo.student_list['10103'].classes_taken) != 0)
+        self.assertTrue(len(repo.instructor_list['98765'].courses_taught) != 0)
 
-        
+    def test_loadMajors(self):
+        repo = f.Repository()
+        repo.loadUserInfo('instructors.txt', 'I')
+        repo.loadUserInfo('students.txt', 'S')
+        repo.loadMajors('majors.txt')
+        self.assertTrue(len(repo.major_dict.keys()) != 0)
         
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity = 2)
